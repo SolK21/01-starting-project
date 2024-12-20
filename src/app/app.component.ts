394c2dca35b6618423e157ffbb15a9bdc051b05f
Konstantinos Solakis <konstantinos.solakis@binwiz.dev>
2024-12-20 23:45:53 +0200

    20-12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,31 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from "./tasks/tasks.component";
+
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ 
+  imports: [
     HeaderComponent,
-    UserComponent
-  ],
+    UserComponent,
+    TasksComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  userId = '';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id == this.userId);
+  }
+
+  onSelectUser(uid:string) {
+    this.userId = uid
+    const selectedUser = DUMMY_USERS.find((user) => user.id == uid);
+  }
+}
